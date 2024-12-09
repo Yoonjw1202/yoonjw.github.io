8b5866220479287580db25b41ea015390fe47edc
Yoonjw1202 <yab3481@naver.com>
2024-12-09 12:15:49 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 
+
+// Select the card container
+const cardContainer = document.querySelector('.card-container');
+
+// Add a click event to toggle the "clicked" class
+cardContainer.addEventListener('click', () => {
+  cardContainer.classList.toggle('clicked');
+});
